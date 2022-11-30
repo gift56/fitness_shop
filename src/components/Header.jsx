c7db44b25c2f8cd58d9d329c8b768391ab445482
gift56 <efeasiughu@gmail.com>
2022-11-30 16:10:15 +0100

    imported header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Header = () => {
-  return <header className="sticky top-0 bg-[#FAFAFA] z-10">Header</header>;
+  return (
+    <header className="sticky top-0 bg-[#FAFAFA] z-10 h-[50px]">Header</header>
+  );
 };
 
 export default Header;
