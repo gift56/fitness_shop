a0688407509c5d54f0016a8e6a47b444397d4463
gift56 <efeasiughu@gmail.com>
2022-11-30 17:27:18 +0100

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="bg-white">
-      <div className="container py-6">
+      <div className="container py-8">
         <div className="w-full flex items-center justify-between h-[364px] bg-[#F3C5FF] px-4">
           <div></div>
           <div></div>
