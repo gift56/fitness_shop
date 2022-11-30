d45982ff03c1038879c015a8b0fe9647237cccec
gift56 <efeasiughu@gmail.com>
2022-11-30 16:24:38 +0100

    installed react bullshit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 bg-[#FAFAFA] z-10 h-[50px]">
+    <header className="sticky top-0 bg-[#FAFAFA] z-10">
       <div className="container">
-        <div className="flex justify-between items-center w-full">
-          <h2 className="text-primary">My Store</h2>
+        <div className="flex justify-between items-center w-full py-4">
+          <h2 className="text-black font-semibold  text-2xl">
+            <Link to="home" spy={true} smooth={true}>
+              My <span>Store</span>
+            </Link>
+          </h2>
         </div>
       </div>
     </header>
