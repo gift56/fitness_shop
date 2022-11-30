bbf50c2484986a0243d282fef6dfcaf7e42c07e5
gift56 <efeasiughu@gmail.com>
2022-11-30 17:30:03 +0100

    working on hero section

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ text, btnClass }) => {
+  return <div>{text}</div>;
+};
 
-export default Button
+export default Button;
