47693215147b0adc798c0730aa9cbe2ed7bfde12
gift56 <efeasiughu@gmail.com>
2022-11-30 16:30:24 +0100

    added links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import { Link } from "react-scroll";
 
 const Header = () => {
+  const scrollLinks = [
+    {
+      href: "home",
+      text: "Home",
+    },
+    {
+      href: "men",
+      text: "Men",
+    },
+    {
+      href: "women",
+      text: "Women",
+    },
+    {
+      href: "sale",
+      text: "Sale",
+    },
+  ];
   return (
     <header className="sticky top-0 bg-[#FAFAFA] z-10">
       <div className="container">
         <div className="flex justify-between items-center w-full py-4">
           <h2 className="text-black font-semibold lg:text-2xl text-xl">
             <Link to="home" spy={true} smooth={true}>
-              My <span className="text-primary">Store</span>
+              My <span className="text-primary">Store.</span>
             </Link>
           </h2>
+          <nav className="flex items-center gap-3">
+            <Link to="" spy={true} smooth={true}></Link>
+          </nav>
         </div>
       </div>
     </header>
