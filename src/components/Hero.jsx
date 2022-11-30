64752789f03517f55666c3e6eff8d0bd11d7c9e1
gift56 <efeasiughu@gmail.com>
2022-11-30 17:26:52 +0100

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,10 @@ const Hero = () => {
   return (
     <section className="bg-white">
       <div className="container py-6">
-        <div className="w-full flex"></div>
+        <div className="w-full flex items-center justify-between h-[364px] bg-[#F3C5FF] px-4">
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </section>
   );
