60690a8e49e3c2de102074996e1867bca33b0ab4
gift56 <efeasiughu@gmail.com>
2022-11-30 17:29:32 +0100

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
         <div className="w-full flex items-center justify-between h-[364px] bg-[#F3C5FF] px-4">
           <div className="flex flex-col gap-3 justify-start items-start flex-1">
             <h1>
-              Exercise more and more with MyShop. Number #1 trusted fitness tool
-              website
+              Exercise more and more with <span>MyShop. Number #1</span> trusted
+              fitness tool website
             </h1>
           </div>
           <div className="flex-1"></div>
