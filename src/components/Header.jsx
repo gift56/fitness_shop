5eb2457b58c666296ab5ca535d51798092c53202
gift56 <efeasiughu@gmail.com>
2022-11-30 16:25:46 +0100

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <header className="sticky top-0 bg-[#FAFAFA] z-10">
       <div className="container">
         <div className="flex justify-between items-center w-full py-4">
-          <h2 className="text-black font-semibold  text-2xl">
+          <h2 className="text-black font-semibold lg:text-2xl text-xl">
             <Link to="home" spy={true} smooth={true}>
-              My <span>Store</span>
+              My <span className="text-primary">Store</span>
             </Link>
           </h2>
         </div>
