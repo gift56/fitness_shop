59a4c9bc881fd0093ba8407c99e807c9223fe0c0
gift56 <efeasiughu@gmail.com>
2022-11-30 16:08:04 +0100

    imported header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
