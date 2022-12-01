7885c54e6c99b8892475643977663d3cf6bf6a9e
gift56 <efeasiughu@gmail.com>
2022-12-01 17:30:26 +0100

    working on media screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,40 +8,40 @@ import Button from "./button/Button";
 
 const Hero = () => {
   return (
-    <section className="bg-white ">
-      <div className="container py-10">
-        <div className="w-full flex items-center justify-between h-[383px] bg-[#F3C5FF] px-4 relative">
-          <div className="flex flex-col gap-3 justify-start items-start flex-1  pl-10">
-            <h1 className="font-normal text-black leading-[40px] text-[33px] text-left max-w-[503px]">
-              Exercise more and more with{" "}
-              <span className="font-semibold">MyShop. Number #1</span> trusted
+    <section className="lg:bg-white bg-[#F3C5FF]">
+      <div className="container lg:py-10 pt-8">
+        <div className="w-full flex lg:flex-row flex-col gap-4 items-center justify-between lg:h-[383px] bg-[#F3C5FF] lg:px-4 relative">
+          <div className="flex flex-col gap-3 justify-start items-start flex-1 lg:pl-10">
+            <h1 className="font-normal text-black leading-[40px] text-[25px] md:text-[33px] text-left lg:max-w-[503px] w-full">
+              Exercise more and more with
+              <span className="font-semibold"> MyShop. Number #1</span> trusted
               fitness tool website
             </h1>
             <Button text="View More" btnClass="min-w-[153px] bg-white" />
           </div>
-          <div className="flex-1">
-            <div className="absolute top-[-100px] right-[65px] h-[500px] z-10">
+          <div className="flex-1 relative">
+            <div className="lg:absolute lg:top-[-100px] lg:right-[65px] md:h-[500px] z-10 w-full relative">
               <img
                 src={HeroImg}
                 alt="heroImage"
-                className="h-full object-cover translate-y-[-1rem]"
+                className="h-full object-cover lg:translate-y-[-1rem]"
               />
             </div>
-            <div className="absolute top-[145px] right-[505px] w-[65px] h-[65px] z-[9]">
+            <div className="hidden lg:flex lg:absolute top-[145px] right-[505px] w-[65px] h-[65px] z-[9]">
               <img
                 src={balls1}
                 alt="/"
                 className="w-full h-full object-contain"
               />
             </div>
-            <div className="absolute top-[40px] right-[355px] w-[86px] h-[86px] z-[9]">
+            <div className="hidden lg:flex lg:absolute top-[40px] right-[355px] w-[86px] h-[86px] z-[9]">
               <img
                 src={balls2}
                 alt="/"
                 className="w-full h-full object-contain"
               />
             </div>
-            <div className="absolute bottom-0 right-[330px] w-[125px] h-[125px] z-[9]">
+            <div className="hidden lg:flex lg:absolute bottom-0 right-[330px] w-[125px] h-[125px] z-[9]">
               <img
                 src={balls3}
                 alt="/"
