32adbc0a0d186e5535a424ec40482cd88b9e9e1f
gift56 <efeasiughu@gmail.com>
2022-12-01 16:53:43 +0100

    positioning image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <section className="bg-white ">
       <div className="container py-10">
         <div className="w-full flex items-center justify-between h-[383px] bg-[#F3C5FF] px-4 relative">
-          <div className="flex flex-col gap-3 justify-start items-start flex-1">
+          <div className="flex flex-col gap-3 justify-start items-start flex-1  pl-10">
             <h1 className="font-normal text-black leading-[40px] text-[33px] text-left max-w-[503px]">
               Exercise more and more with{" "}
               <span className="font-semibold">MyShop. Number #1</span> trusted
@@ -20,7 +20,7 @@ const Hero = () => {
             <Button text="View More" btnClass="min-w-[153px] bg-white" />
           </div>
           <div className="flex-1">
-            <div className="absolute top-[-100px] right-[40px] h-[500px]">
+            <div className="absolute top-[-100px] right-[65px] h-[500px]">
               <img
                 src={HeroImg}
                 alt="heroImage"
