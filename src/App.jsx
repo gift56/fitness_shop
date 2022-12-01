369178df74acb2b527b8cb19213125aeaa16b2aa
gift56 <efeasiughu@gmail.com>
2022-12-01 21:13:42 +0100

    done with desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CartCards from "./components/CartCards";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
 import ToolsCards from "./components/ToolsCards";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <ToolsCards />
       <CartCards />
+      <Testimonials />
     </>
   );
 };
