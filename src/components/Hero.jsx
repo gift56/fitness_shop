86562d3181452c77979018c1af0e6d1edc950159
gift56 <efeasiughu@gmail.com>
2022-12-01 16:36:43 +0100

    styling h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div className="container py-8">
         <div className="w-full flex items-center justify-between h-[364px] bg-[#F3C5FF] px-4">
           <div className="flex flex-col gap-3 justify-start items-start flex-1">
-            <h1>
+            <h1 className="font-normal text-black ">
               Exercise more and more with <span>MyShop. Number #1</span> trusted
               fitness tool website
             </h1>
