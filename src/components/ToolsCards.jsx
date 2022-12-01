ef9c708a2a148b41a29ef54258b48e864eb09439
gift56 <efeasiughu@gmail.com>
2022-12-01 18:23:39 +0100

    styling cards

--- src/components/ToolsCards.jsx
+++ src/components/ToolsCards.jsx
@@ -20,12 +20,12 @@ const ToolsCards = () => {
   ];
   return (
     <section className="bg-white">
-      <div className="container pb-[4rem]">
-        <div className="flex flex-col lg:flex-row items-center justify-between gap-4 w-full">
+      <div className="container lg:py-16 py-10">
+        <div className="flex flex-col lg:flex-row items-center justify-between lg:gap-4 gap-7 w-full">
           {cardsData.map(({ img, title, details }, i) => (
             <div
               key={i}
-              className="w-[50%] bg-[#FAFAFA] shadow-large h-[301px] rounded-xl flex items-center justify-between gap-2"
+              className="lg:w-[50%] w-full bg-[#FAFAFA] shadow-large lg:h-[301px] rounded-xl flex flex-col-reverse lg:flex-row items-center justify-between gap-2"
             >
               <div className="flex flex-col justify-between items-start h-full">
                 <h2 className="text-black font-semibold text-[29px] leading-9">
@@ -39,8 +39,8 @@ const ToolsCards = () => {
                   btnClass="min-w-[153px] bg-[#F9F871]"
                 />
               </div>
-              <div>
-                <img src={img} alt={title} />
+              <div className="w-full h-full lg:w-fit lg:h-fit">
+                <img src={img} alt={title} className="w-full h-full" />
               </div>
             </div>
           ))}
