0a1db72c9c4d7fe6da2e57b63a1b9eee667df393
gift56 <efeasiughu@gmail.com>
2022-12-01 19:04:14 +0100

    created card datas

--- src/components/CartCards.jsx
+++ src/components/CartCards.jsx
@@ -15,6 +15,41 @@ const CartCards = () => {
       title: "Speed Rope-Blue",
       rate: "Rs. 800",
     },
+    {
+      img: img2,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 300",
+    },
+    {
+      img: img3,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 1500",
+    },
+    {
+      img: img4,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 600",
+    },
+    {
+      img: img5,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 1300",
+    },
+    {
+      img: img6,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 1300",
+    },
+    {
+      img: img7,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 2000",
+    },
+    {
+      img: img8,
+      title: "Speed Rope-Blue",
+      rate: "Rs. 600",
+    },
   ];
   return (
     <section className="bg-white">
