9ed6f15efd6acda7d37191a5d912346a591e67b3
gift56 <efeasiughu@gmail.com>
2022-12-01 17:53:13 +0100

    working on cards

--- src/components/ToolsCards.jsx
+++ src/components/ToolsCards.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Shoe from "../assets/whiteShoe.svg";
+import GymTool from "../assets/dumbell.svg";
 
 const ToolsCards = () => {
+  const cardsData = [
+    {
+      img: Shoe,
+      title: "Mens Training Shoe",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue orci, pretium nibh eget.",
+    },
+  ];
   return (
-    <section>ToolsCards</section>
-  )
-}
+    <section className="bg-white">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-4 w-full"></div>
+      </div>
+    </section>
+  );
+};
 
-export default ToolsCards
+export default ToolsCards;
