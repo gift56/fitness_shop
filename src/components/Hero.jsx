c2edb06299b5a38ab990cf0fa9bb32a2db6afccd
gift56 <efeasiughu@gmail.com>
2022-12-01 16:46:23 +0100

    positioning image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,17 +8,22 @@ import Button from "./button/Button";
 
 const Hero = () => {
   return (
-    <section className="bg-white">
-      <div className="container py-8">
-        <div className="w-full flex items-center justify-between h-[364px] bg-[#F3C5FF] px-4">
+    <section className="bg-white ">
+      <div className="container py-8 bg-black">
+        <div className="w-full flex items-center justify-between h-[394px] bg-[#F3C5FF] px-4 relative">
           <div className="flex flex-col gap-3 justify-start items-start flex-1">
-            <h1 className="font-normal text-black leading-[33px] text-[33px] text-left max-w-[503px]">
-              Exercise more and more with <span>MyShop. Number #1</span> trusted
+            <h1 className="font-normal text-black leading-[40px] text-[33px] text-left max-w-[503px]">
+              Exercise more and more with{" "}
+              <span className="font-semibold">MyShop. Number #1</span> trusted
               fitness tool website
             </h1>
             <Button text="View More" btnClass="min-w-[153px] bg-white" />
           </div>
-          <div className="flex-1 relative"></div>
+          <div className="flex-1">
+            <div className="absolute top-[-100px] right-[40px]">
+              <img src={HeroImg} alt="heroImage" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
