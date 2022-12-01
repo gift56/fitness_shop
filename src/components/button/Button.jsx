b981f3534cf4bfea32a976d214e32d0079be89ed
gift56 <efeasiughu@gmail.com>
2022-12-01 16:27:20 +0100

    working on hero sections

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text, btnClass }) => {
   return (
-    <button className={`h-[48px] flex items-center justify-center outline-none ${btnClass}`}>
+    <button className={`h-[48px] flex items-center justify-center outline-none border-none ${btnClass}`}>
       {text}
     </button>
   );
