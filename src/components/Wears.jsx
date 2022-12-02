7ff26d22cb27a2cf4f46bfe92e37a0782927994e
gift56 <efeasiughu@gmail.com>
2022-12-02 12:18:31 +0100

    working on wear

--- src/components/Wears.jsx
+++ src/components/Wears.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import men1 from "../assets/men1.svg";
 import men2 from "../assets/men2.svg";
 import men3 from "../assets/men3.svg";
+import Wear from "./Wear";
 
 const Wears = () => {
-  return <section>Wears</section>;
+  return (
+    <section className="bg-white">
+      <div className="conatiner py-5">
+        <div className="flex flex-col items-start justify-start w-full gap-5">
+          <Wear />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Wears;
