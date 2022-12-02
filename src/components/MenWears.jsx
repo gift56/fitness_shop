311ada5905dcf82400d5699109e220f5d73122f7
gift56 <efeasiughu@gmail.com>
2022-12-02 12:09:52 +0100

    working on mens wears

--- src/components/MenWears.jsx
+++ src/components/MenWears.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import men1 from "../assets/men1.svg";
+import men2 from "../assets/men2.svg";
+import men3 from "../assets/men3.svg";
 
 const MenWears = () => {
   return <section id="men">MenWears</section>;
