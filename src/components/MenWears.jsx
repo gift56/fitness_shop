f97df7e4035d4f28200d706364204cd94c939c0f
gift56 <efeasiughu@gmail.com>
2022-12-02 12:09:37 +0100

    working on mens wears

--- src/components/MenWears.jsx
+++ src/components/MenWears.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import men1 from ''
+import React from "react";
+import men1 from "../assets/men1.svg";
 
 const MenWears = () => {
-  return (
-    <section id='men'>MenWears</section>
-  )
-}
+  return <section id="men">MenWears</section>;
+};
 
-export default MenWears
+export default MenWears;
