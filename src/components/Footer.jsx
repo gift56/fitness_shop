ad217e1fe83a290e821d57de43a9b290e697f0f0
gift56 <efeasiughu@gmail.com>
2022-12-02 13:53:46 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
     <footer className="bg-[#F3C5FF]">
-      <div className="container py-10">
+      <div className="container py-16">
         <div className="flex w-full flex-wrap justify-between items-start gap-4">
           <div className="flex flex-col gap-8 items-start justify-start flex-1">
-            
+            <h2 className="text-black font-semibold lg:text-[40px] text-xl cursor-pointer">
+              <Link to="home" spy={true} smooth={true}>
+                My <span className="text-primary">Store.</span>
+              </Link>
+            </h2>
           </div>
         </div>
       </div>
