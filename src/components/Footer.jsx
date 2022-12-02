84c71db636ca283108dd66444eaef2b6a40daa7c
gift56 <efeasiughu@gmail.com>
2022-12-02 14:26:00 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#F3C5FF]">
       <div className="container md:py-16 py-12">
-        <div className="flex w-full flex-wrap justify-between items-start gap-4">
+        <div className="flex w-full sm:flex-row flex-col justify-between items-start gap-4">
           <div className="flex flex-col gap-8 items-start justify-start flex-1">
             <h2 className="text-black font-semibold lg:text-[40px] text-2xl cursor-pointer">
               <Link to="home" spy={true} smooth={true}>
