9cb35786b140341e4b8a346b8d4a560b6f366b7a
gift56 <efeasiughu@gmail.com>
2022-12-02 12:45:53 +0100

    working on wear

--- src/components/Wear.jsx
+++ src/components/Wear.jsx
@@ -15,18 +15,22 @@ const Wear = ({ id, title, details, btn, data }) => {
         </p>
         {btn}
       </div>
-      <div className="flex gap-6 items-start justify-start flex-1">
+      <div className="flex flex-col lg:flex-row gap-6 items-start justify-start flex-1 w-full">
         {data.map(({ img, name, price }, i) => (
           <div
             key={i}
-            className="flex justify-start min-w-[233px] items-center flex-col gap-5 bg-[#E1DADA] pb-7"
+            className="flex justify-start lg:min-w-[233px] w-full items-center flex-col gap-5 bg-[#E1DADA] pb-7"
           >
             <div className="lg:w-fit lg:h-fit w-full h-full">
               <img src={img} alt={name} className="w-full h-full" />
             </div>
             <div className="flex flex-col items-start justify-start w-full gap-3">
-              <h3 className="text-black text-xl font-medium">{name}</h3>
-              <p className="text-black text-xl font-medium">{price}</p>
+              <h3 className="text-black text-lg lg:text-xl font-medium">
+                {name}
+              </h3>
+              <p className="text-black text-lg lg:text-xl font-medium">
+                {price}
+              </p>
             </div>
           </div>
         ))}
