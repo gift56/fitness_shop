00f5e6ca6da938487bf4a0dfbda946e9c2f708cf
gift56 <efeasiughu@gmail.com>
2022-12-02 13:57:37 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-scroll";
 
 const Footer = () => {
+  const footerRows = [{}];
+  
   return (
     <footer className="bg-[#F3C5FF]">
       <div className="container md:py-16 py-12">
@@ -12,7 +14,15 @@ const Footer = () => {
                 My <span className="text-primary">Store.</span>
               </Link>
             </h2>
+            <p className="text-[#504A41] lg:text-lg text-base font-normal">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
+              malesuada dapibus ut pulvinar neque arcu, commodo. Pharetra nisi
+              egestas nisi fermentum. Sollicitudin egestas senectus pellentesque
+              enim mauris vel odio commodo. Pellentesque orci vestibulum sed in
+              molestie consequat.
+            </p>
           </div>
+          <div className="flex flex-wrap gap-8 items-start justify-start flex-1"></div>
         </div>
       </div>
     </footer>
