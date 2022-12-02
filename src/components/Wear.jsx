08ecb257e381c44a1dc148c2423c7aca2b0af8d2
gift56 <efeasiughu@gmail.com>
2022-12-02 12:26:35 +0100

    working on wears

--- src/components/Wear.jsx
+++ src/components/Wear.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Wear = ({ id, title, details, btn, data }) => {
-  return <div>Wear</div>;
+  return (
+    <div
+      id={id}
+      className="w-full flex flex-col lg:flex-row items-start justify-between gap-6"
+    ></div>
+  );
 };
 
 export default Wear;
