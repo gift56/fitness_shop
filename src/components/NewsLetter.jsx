4c3428aab3334cf7de5d53679c41fe187555529c
gift56 <efeasiughu@gmail.com>
2022-12-02 13:37:52 +0100

    working on newsletter

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -4,8 +4,8 @@ import Button from "./button/Button";
 const NewsLetter = () => {
   return (
     <section className="lg:bg-white bg-[#F3C5FF]" id="login">
-      <div className="container lg:py-10 pt-8">
-        <div className="w-full flex lg:flex-row flex-col gap-4 items-center justify-center lg:h-[303px] bg-[#EED9C8] lg:px-4 relative">
+      <div className="container py-10">
+        <div className="w-full flex lg:flex-row flex-col gap-4 items-center justify-center lg:h-[303px] bg-[#EED9C8] lg:px-4 relative p-5">
           <div className="flex flex-col gap-3 justify-start items-start flex-1 lg:pl-10">
             <h1 className="font-normal text-black leading-[40px] text-[25px] md:text-[33px] text-left lg:max-w-[503px] w-full">
               Join Our News Letter
@@ -23,7 +23,7 @@ const NewsLetter = () => {
               type="text"
               name="newsletter"
               id="newsletter"
-              className="h-[55px] outline-none border-none w-[440px] pl-4 text-xl"
+              className="h-[55px] outline-none border-none lg:w-[440px] w-full pl-4 text-xl"
             />
             <Button
               text="Subscribe"
