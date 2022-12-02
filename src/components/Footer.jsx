12e101bdeef89d0e6518411572ca7b1f873d3e46
gift56 <efeasiughu@gmail.com>
2022-12-02 13:52:09 +0100

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="bg-[#F3C5FF]"></footer>;
+  return (
+    <footer className="bg-[#F3C5FF]">
+      <div className="container py-10">
+        <div className="flex w-full flex-wrap justify-between items-start gap-4">
+          <div className="flex flex-col gap-8 items-start justify-start flex-1">
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
