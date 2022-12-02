24e8507d46bfdafd8e82799f2ee8cfb117ed641d
gift56 <efeasiughu@gmail.com>
2022-12-02 12:27:44 +0100

    working on wears

--- src/components/Wear.jsx
+++ src/components/Wear.jsx
@@ -5,7 +5,9 @@ const Wear = ({ id, title, details, btn, data }) => {
     <div
       id={id}
       className="w-full flex flex-col lg:flex-row items-start justify-between gap-6"
-    ></div>
+    >
+        <div className="flex flex-col gap-6 items-start justify-start"></div>
+    </div>
   );
 };
 
