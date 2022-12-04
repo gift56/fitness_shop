c5e6f26b699dce5f54037e8a2742f01aa4f454c6
gift56 <efeasiughu@gmail.com>
2022-12-04 04:24:06 +0100

    working on animation

--- src/components/Wear.jsx
+++ src/components/Wear.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const Wear = ({
@@ -16,36 +17,66 @@ const Wear = ({
       className={`w-full flex flex-col lg:flex-row items-start justify-between gap-6 ${className}`}
     >
       <div className="flex flex-col gap-6 items-start justify-start flex-1">
-        <h1 className="text-black font-bold text-[56px] leading-[72px]">
+        <motion.h1
+          initial={{ opacity: 0, x: "-60px" }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2, type: "tween" }}
+          className="text-black font-bold text-[56px] leading-[72px]"
+        >
           {title}
-        </h1>
-        <p className="text-[19px] leading-[23px] text-black font-normal max-w-sm">
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: "60px" }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2, type: "tween" }}
+          className="text-[19px] leading-[23px] text-black font-normal max-w-sm"
+        >
           {details}
-        </p>
+        </motion.p>
         {btn}
       </div>
       <div
         className={`flex flex-col lg:flex-row gap-6 items-start justify-start flex-1 w-full ${dataClasses}`}
       >
         {data.map(({ img, name, price }, i) => (
-          <div
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 3, type: "tween" }}
             key={i}
             className={`flex justify-start lg:min-w-[30%] w-full items-center flex-col gap-5 bg-[#E1DADA] pb-7 ${dataClasses}`}
           >
             <div className="lg:w-fit lg:h-fit w-full h-full">
-              <img src={img} alt={name} className="w-full h-full max-w-full" />
+              <motion.img
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 3, type: "tween" }}
+                src={img}
+                alt={name}
+                className="w-full h-full max-w-full"
+              />
             </div>
             {detail === false ? (
-              <div className="flex flex-col items-start justify-start w-full gap-3">
-                <h3 className="text-black text-lg lg:text-xl font-medium">
+              <div className="flex flex-col items-start justify-start w-full gap-3 overflow-hidden">
+                <motion.h3
+                  initial={{ opacity: 0, x: "-30px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 2, type: "tween" }}
+                  className="text-black text-lg lg:text-xl font-medium"
+                >
                   {name}
-                </h3>
-                <p className="text-black text-lg lg:text-xl font-medium">
+                </motion.h3>
+                <motion.p
+                  initial={{ opacity: 0, x: "-30px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 2, type: "tween" }}
+                  className="text-black text-lg lg:text-xl font-medium"
+                >
                   {price}
-                </p>
+                </motion.p>
               </div>
             ) : null}
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
