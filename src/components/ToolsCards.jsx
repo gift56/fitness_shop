4c70a9805c83dd1ad6539e01e291f0dbd1e44d71
gift56 <efeasiughu@gmail.com>
2022-12-04 04:23:49 +0100

    working on animation

--- src/components/ToolsCards.jsx
+++ src/components/ToolsCards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Shoe from "../assets/whiteShoe.svg";
 import GymTool from "../assets/dumbell.svg";
 import Button from "./button/Button";
+import { motion } from "framer-motion";
 
 const ToolsCards = () => {
   const cardsData = [
@@ -19,30 +20,52 @@ const ToolsCards = () => {
     },
   ];
   return (
-    <section className="bg-white">
+    <section className="bg-white overflow-hidden">
       <div className="container lg:py-16 py-10">
         <div className="flex flex-col lg:flex-row items-center justify-between lg:gap-4 gap-7 w-full">
           {cardsData.map(({ img, title, details }, i) => (
-            <div
+            <motion.div
+              initial={{ opacity: 0, x: `${i === 0 ? "-100px" : "100px"}` }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2, type: "tween" }}
+              whileHover={{ scale: 1.02 }}
               key={i}
-              className="lg:w-[50%] w-full bg-[#FAFAFA] shadow-large lg:h-[301px] rounded-xl flex flex-col-reverse lg:flex-row items-center justify-between gap-2"
+              className="lg:w-[50%] w-full bg-[#FAFAFA] shadow-large lg:h-[301px] rounded-xl flex flex-col-reverse lg:flex-row items-center justify-between gap-2 cursor-pointer"
             >
-              <div className="flex flex-col justify-between gap-4 lg:gap-0 items-start h-full">
-                <h2 className="text-black font-semibold sm:text-[29px] text-2xl leading-9">
+              <div className="flex flex-col justify-between gap-4 lg:gap-0 items-start h-full overflow-hidden">
+                <motion.h2
+                  initial={{ opacity: 0, y: "-25px" }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 3, type: "tween" }}
+                  className="text-black font-semibold sm:text-[29px] text-2xl leading-9"
+                >
                   {title}
-                </h2>
-                <p className="sm:text-xl text-base font-light text-black w-full lg:w-[279px]">
+                </motion.h2>
+                <motion.p
+                  initial={{ opacity: 0, x: "-30px" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{ duration: 4, type: "tween" }}
+                  className="sm:text-xl text-base font-light text-black w-full lg:w-[279px]"
+                >
                   {details}
-                </p>
+                </motion.p>
                 <Button
+                  time={3}
                   text="View More"
                   btnClass="min-w-[153px] bg-[#F9F871]"
                 />
               </div>
               <div className="w-full h-full lg:w-fit lg:h-fit">
-                <img src={img} alt={title} className="w-full h-full" />
+                <motion.img
+                  initial={{ opacity: 0 }}
+                  whileInView={{ opacity: 1 }}
+                  transition={{ duration: 2, type: "tween" }}
+                  src={img}
+                  alt={title}
+                  className="w-full h-full hover:transform"
+                />
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
