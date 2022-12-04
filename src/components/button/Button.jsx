02ff3345fe8fa2e4b8006446c6b34958682c1084
gift56 <efeasiughu@gmail.com>
2022-12-04 04:25:54 +0100

    working on animation

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,12 +1,16 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-const Button = ({ text, btnClass }) => {
+const Button = ({ text, btnClass, time }) => {
   return (
-    <button
+    <motion.button
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: time }}
       className={`h-[48px] flex items-center justify-center outline-none rounded-xl cursor-pointer text-black font-medium text-xl hover:opacity-[0.8] transition-all duration-300 ${btnClass}`}
     >
       {text}
-    </button>
+    </motion.button>
   );
 };
 
