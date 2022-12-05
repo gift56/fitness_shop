668c945febff1d4267f3617f2d456ca563105ba4
gift56 <efeasiughu@gmail.com>
2022-12-05 22:26:16 +0100

    working on redux

--- src/components/CartCards.jsx
+++ src/components/CartCards.jsx
@@ -12,48 +12,7 @@ import Button from "./button/Button";
 import { motion } from "framer-motion";
 
 const CartCards = () => {
-  const cartCards = [
-    {
-      img: img1,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 800",
-    },
-    {
-      img: img2,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 300",
-    },
-    {
-      img: img3,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 1500",
-    },
-    {
-      img: img4,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 600",
-    },
-    {
-      img: img5,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 1300",
-    },
-    {
-      img: img6,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 1300",
-    },
-    {
-      img: img7,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 2000",
-    },
-    {
-      img: img8,
-      title: "Speed Rope-Blue",
-      rate: "Rs. 600",
-    },
-  ];
+ 
   const opacity = (id) => {
     if (id === 0 || id === 4) {
       return "-150px";
---
+++ src/utils/cartData.js
@@ -0,0 +1,42 @@
+export const cartCards = [
+  {
+    img: img1,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 800",
+  },
+  {
+    img: img2,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 300",
+  },
+  {
+    img: img3,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 1500",
+  },
+  {
+    img: img4,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 600",
+  },
+  {
+    img: img5,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 1300",
+  },
+  {
+    img: img6,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 1300",
+  },
+  {
+    img: img7,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 2000",
+  },
+  {
+    img: img8,
+    title: "Speed Rope-Blue",
+    rate: "Rs. 600",
+  },
+];
